3d04174b9a0403c620fd14499b077df00b1eeaeb
DYST0P1A <pablogomeztic95@gmail.com>
2021-06-06 19:45:12 +0200

    views added

--- src/views/BuyAndSell.vue
+++ src/views/BuyAndSell.vue
@@ -1,13 +1,78 @@
 <template>
-  <h1>COMPRAVENTA</h1>
+  <b-row>
+    <b-col cols="8">
+      <b-row>
+        <b-card-group>
+          <b-col
+            cols="4"
+            v-for="item in $store.state.productsUsed"
+            :key="item._id"
+          >
+            <b-card
+              bg-variant="light"
+              border-variant="dark"
+              :header="item.name"
+              :img-src="item.images[0].image"
+              class="mb-md-2"
+            >
+              <b-card-text class="font-weight-bold" align="center">
+                VENDEDOR: {{ item.nameSeller }} 
+              </b-card-text>
+
+              <b-card-text class="font-weight-bold" align="right">
+                {{ item.price }} €
+              </b-card-text>
+
+              <router-link
+                :to="{ name: 'ProductDetail', params: { id: item._id } }"
+              >
+                <b-button class="mr-3" variant="primary">View</b-button>
+              </router-link>
+
+              <b-button
+                variant="primary"
+                @click="$store.dispatch('addToCart', item)"
+                >Add</b-button
+              >
+
+              <b-button
+                class="ml-2"
+                variant="danger"
+                @click="$store.dispatch('decrementProduct', item.id)"
+                >-</b-button
+              >
+            </b-card>
+          </b-col>
+        </b-card-group>
+      </b-row>
+    </b-col>
+    <b-col>
+      <CartProduct />
+    </b-col>
+  </b-row>
 </template>
 
 <script>
-export default {
+import store from "../store/index.js";
+import CartProduct from "../components/CartProduct.vue";
 
-}
+export default {
+  name: "Product",
+  components: {
+    CartProduct,
+  },
+  beforeCreate() {
+    store.dispatch("fetchProductsUsedData");
+  },
+};
 </script>
 
 <style>
+.product-name {
+  font-size: 15px;
+}
 
+.product-price {
+  font-size: 20px;
+}
 </style>
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,39 +1,70 @@
 <template>
-      <div>
-          <h1>PRODUCTOS</h1>
-        <div class="card-product" v-for="item in $store.state.products" :key="item.id">
-          <img :src="item.image" class="product-img" />
-          <span class="product-name">
-            {{ item.name }} 
-          </span>
-          <span class="product-price">
-            {{ item.price }}€
-          </span>
-        <div class="controls">
-            <button class="btn btn-tribe">View</button>
-            <button class="btn btn-tribe" @click="$store.dispatch('addToCart', item.id)">Add to cart</button>
-          </div>
-        </div>
-    </div>
+  <b-row>
+    <b-col cols="8">
+      <b-row>
+        <b-card-group>
+          <b-col cols="4" v-for="item in $store.state.products" :key="item._id">
+            <b-card
+              bg-variant="light"
+              border-variant="dark"
+              :header="item.name"
+              :img-src="item.images[0].image"
+              class="mb-md-2"
+            >
+              <b-card-text class="font-weight-bold" align="right">
+                {{ item.price }} €
+              </b-card-text>
+
+              <router-link
+                :to="{ name: 'ProductDetail', params: { id: item._id } }"
+              >
+                <b-button class="mr-3" variant="primary">View</b-button>
+              </router-link>
+
+              <b-button
+                variant="primary"
+                @click="$store.dispatch('addToCart', item)"
+                >Add</b-button
+              >
+
+              <b-button
+                class="ml-2"
+                variant="danger"
+                @click="$store.dispatch('decrementProduct', item.id)"
+                >-</b-button
+              >
+            </b-card>
+          </b-col>
+        </b-card-group>
+      </b-row>
+    </b-col>
+    <b-col>
+      <CartProduct />
+    </b-col>
+  </b-row>
 </template>
 
 <script>
+import store from "../store/index.js";
+import CartProduct from "../components/CartProduct.vue";
+
 export default {
-  name: 'App',
-  created() {
-      this.$store.dispatch('fetchProductsData')
-  }
-}
+  name: "Product",
+  components: {
+    CartProduct
+  },
+  beforeCreate() {
+    store.dispatch("fetchProductsData");
+  },
+};
 </script>
 
 <style>
-
-.product-name{
-    font-size: 15px;
+.product-name {
+  font-size: 15px;
 }
 
-.product-price{
-    font-size: 20px;
+.product-price {
+  font-size: 20px;
 }
-
 </style>
